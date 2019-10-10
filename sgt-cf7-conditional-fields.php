08355390e0f04a1a4d9b566b9459a910e5872604
Scott Tirrell <STirrell@MEMIC.COM>
2019-10-10 15:46:26 -0400

    Initial check-in of files.

---
+++ sgt-cf7-conditional-fields.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Conditional Required Fields for Contact Form 7
+Plugin URI: https://second-cup-of-coffee.com/
+Description: Create conditional required fields in Contact Form 7
+Author: Scott Tirrell
+Author URI: https://second-cup-of-coffee.com/
+Version: 0.0.1
+Text Domain: sgt-cf7-cond-fld
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
